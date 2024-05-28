f50572713b5a0db14fb53e851b8ace343e83c82c
zackartz <zackmyers@lavabit.com>
2024-05-28 17:46:33 -0400

    redesign c:

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
+import syntaxTheme from "./syntax-theme.json";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://zackmyers.io",
   markdown: {
     shikiConfig: {
-      theme: "catppuccin-frappe",
+      theme: syntaxTheme,
     },
   },
   integrations: [mdx(), sitemap(), tailwind()],
